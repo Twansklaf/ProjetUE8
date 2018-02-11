88687f0a40dd2c3a9df5bfd144df7802ceedfa69
twans <antoine.dolant@u-psud.fr>
2018-02-11 19:05:13 +0100

    search in twitter files

--- twitter_db.py
+++ twitter_db.py
@@ -28,22 +28,44 @@ class TwitSearchNoSpark():
 	def __init__(self):
 		self._data = []
 
-	def load_json(self, path):
+	def load_json(self, path, params):
 		
 		with open(path) as f:
 			l = f.readline()
 			while l != "" :
-				self._data.append(json.loads(l))
+				
+				tmp_dict = json.loads(l)
+
+				tab_str = params.split(" ")
+				vect = [0 for x in tab_str]
+
+				for i in range(0, len(tab_str)):
+
+					if tab_str[i] in tmp_dict['text']:
+
+						vect[i] = 1
+
+				if sum(vect) == len(vect):
+
+					self._data.append(tmp_dict)
+
 				l = f.readline()
-		print("Done loading tw database")
+
+		# print("Done loading tw database")
 
 #Creation d'une DATAFRAME (une « table ») à partir du fichier JSON se trouvant dans HDFS
 #Il faut remplacer le chemin par le fichier sous hdfs
 #on pourra le remplacer au début par un simple fichier local
 
-# sp = TwitSearchNoSpark()
+sp = TwitSearchNoSpark()
 
-# sp.load_json("Data/tweets.json")
+sp.load_json("Data/tweets.json", "sushi japonais")
+
+print(sp._data[0]['text'])
+
+# for x in range(2000,2010):
+
+# 	print(sp._data[x]['text'])
 
 # print("loadok")
 
