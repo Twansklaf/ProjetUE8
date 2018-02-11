c532f5821dc69f66a9ff75eeb8e19f7a4a490646
twans <antoine.dolant@u-psud.fr>
2018-02-11 15:28:57 +0100

    Recreated ssh file

---
+++ launch_server.py
@@ -0,0 +1,4 @@
+import twitter_db
+import httpd
+
+
--- twitter_db.py
+++ twitter_db.py
@@ -2,33 +2,55 @@
 
 
 from pyspark.sql import SparkSession
+import json
+
+SPARK_APP_NAME = "TwitSearch"
+
 
 #Creation d'une application spark
 
 class TwitSearch():
 
-	self._sparkengine = None
-	self._dataframe = None
-
 	def __init__(self):
 
-		self._sparkengine = SparkSession.builder.appName(config.SPARK_APP_NAME) \
+		self._sparkengine = SparkSession.builder.appName(SPARK_APP_NAME) \
 		                        .config("spark.ui.showConsoleProgress","false") \
 		                        .master("local").getOrCreate()
-		spark.sparkContext.setLogLevel("ERROR")
+		self._sparkengine.sparkContext.setLogLevel("ERROR")
+		self._dataframe = None
 
+	def stopspark(self):
+		self._sparkengine.stop()
+
+class TwitSearchNoSpark():
+
+	def __init__(self):
+		self._data = []
+
+	def load_json(self, path):
 		
+		with open(path) as f:
+			l = f.readline()
+			while l != "" :
+				self._data.append(json.loads(l))
+				l = f.readline()	
 
 #Creation d'une DATAFRAME (une « table ») à partir du fichier JSON se trouvant dans HDFS
 #Il faut remplacer le chemin par le fichier sous hdfs
 #on pourra le remplacer au début par un simple fichier local
 
-df = spark.read.json("hdfs://localhost:9010/input/twitter/20170129.json")
+sp = TwitSearchNoSpark()
 
-rdd = df.rdd
+sp.load_json("Data/tweets.json")
+
+print("loadok")
+
+# df = sp._sparkengine.read.json("hdfs://localhost:9010/Data/tweets.json")
+
+# rdd = df.rdd
 
 #On obtient un objet RDD de Row(…) qui fonctionnent comme des dictionnaires pythons.
 
-rdd2 = rdd.map(lambda x: x['user_id'])
-for i in rdd2.collect():
-    print (i)
+# rdd2 = rdd.map(lambda x: x['user_id'])
+# for i in rdd2.collect():
+#     print (i)
